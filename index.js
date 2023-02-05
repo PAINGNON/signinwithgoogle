9f884dfa2faec43a17cd04ed3f06c93f481259b1
PAING NON <63273633+PAINGNON@users.noreply.github.com>
2023-02-05 14:08:42 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ function onSignIn(googleUser) {
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display","block");
-    $(".g-signin2").css("display","none");
-  }
-  function signOut() {
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      alert("You have been signed out successfully");
-      $(".g-signin2").css("display","block");
-      $(".data").css("display","none");
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
     });
-  }
+}
